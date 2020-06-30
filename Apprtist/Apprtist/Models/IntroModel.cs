754b1683d73507dd5aaddc7e9fe78edab7f421c5
sanmoa6640 <59384822+sanmoa6640@users.noreply.github.com>
2020-06-30 14:40:31 -0500

    Add CarouselView for the introPage

---
+++ Apprtist/Apprtist/Models/IntroModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apprtist.Models
+{
+    public class IntroModel
+    {
+        public string ImageUrl { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
--- Apprtist/Apprtist/PageModels/IntroPageModel.cs
+++ Apprtist/Apprtist/PageModels/IntroPageModel.cs
@@ -1,7 +1,39 @@
 ﻿
+using Apprtist.Models;
+using System.Collections.ObjectModel;
+
 namespace Apprtist.PageModels
 {
     public class IntroPageModel : BasePageModel
     {
+        ObservableCollection<IntroModel> presentationData;
+        public ObservableCollection<IntroModel> PresentationData
+        {
+            get { return presentationData; }
+            set { presentationData = value; RaisePropertyChanged(); }
+        }
+
+        public IntroPageModel()
+        {
+            LoadPresentationData();
+        }
+
+        private void LoadPresentationData()
+        {
+            PresentationData = new ObservableCollection<IntroModel>
+            {
+                new IntroModel
+                {
+                    ImageUrl = "https://picsum.photos/250?image=9",
+                    Name = "First Presentation"
+                },
+                new IntroModel
+                {
+                    ImageUrl = "https://aka.ms/campus.jpg",
+                    Name = "Second Presentation"
+                }
+            };
+        }
     }
 }
+
